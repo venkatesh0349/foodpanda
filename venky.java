1693f5f36f22ee8e5b2ec1f724725823aa9a0b3b
venkatesh0349 <reddy.venkatesh87@gmail.com>
2017-07-19 20:47:42 +0530

    changes done 20 to 100

--- venky.java
+++ venky.java
@@ -6,7 +6,7 @@ public class Java Program
 		system.out.print("Enter two numbers")
 
 			a=80;
-		        b=60;
+		        b=100;
                         sum= a+b;
 			
 			 System.out.print("sum of the Entered two numbwers " +sum)
