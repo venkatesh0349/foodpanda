1efbe8c5a7c64975677cdcb2638bc4698c4e8aaa
venkatesh0349 <reddy.venkatesh87@gmail.com>
2017-07-19 17:55:13 +0530

    creted a new file venky.java

---
+++ venky.java
@@ -0,0 +1,14 @@
+public class Java Program
+{
+	public status void main (String args[])
+	{
+		int a,b,sum;
+		system.out.print("Enter two numbers")
+			a=30;
+		        b=70;
+
+			sum= a+b;
+			 System.out.print("sum of the Entered two numbwers " +sum)
+	}
+}
+
